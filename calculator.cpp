9d9bb11ec94e4165130b543f6b33545d96a52774
poluidol2 <iliaog2000@gmail.com>
2022-11-02 17:45:50 +0300

    personal changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,9 @@
 #include "calculator.h"
-
+// my comment one
+// my comment two
+// my comment three
+// my comment four
+// my comment five
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
